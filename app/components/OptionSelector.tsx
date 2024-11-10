7ee0b788fab7118d5c0a0a412a2c07b288b124c5
yuchi1128 <yukiya20021128@icloud.com>
2024-11-11 01:30:58 +0900

    feat: 都道府県選択コンポーネントの実装

---
+++ app/components/OptionSelector.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OptionSelector = () => {
+  return (
+    <div>OptionSelector</div>
+  )
+}
+
+export default OptionSelector
---
+++ app/components/PrefecturePopulationChart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrefecturePopulationChart = () => {
+  return (
+    <div>PrefecturePopulationChart</div>
+  )
+}
+
+export default PrefecturePopulationChart
